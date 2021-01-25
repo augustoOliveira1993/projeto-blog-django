9f6763a60bca824c754bbc3e8c3655dd23a10e13
Augusto Silva <gutim160@gmail.com>
2021-01-25 15:48:58 -0300

    Configuracao basicas do django

--- blog/urls.py
+++ blog/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+# Mensagens
+from django.contrib.messages import constants
 
 urlpatterns = [
+    path('', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
