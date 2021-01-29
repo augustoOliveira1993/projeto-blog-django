cea2a082f1988583ba2f05c965f79e4405f58f36
Augusto Silva <gutim160@gmail.com>
2021-01-29 06:41:44 -0300

    finalizando o base.html e corrigindo alguns erros.. rodando e fucionando ok

--- blog/settings.py
+++ blog/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILEs_DIRS = (os.path.join(BASE_DIR, 'static/'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 INSTALLED_APPS += ('django_summernote', )
